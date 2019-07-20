459e28b8e3a5ad1088ec999f9a77367944dd2faa
Brian Faust <hello@basecode.sh>
2019-07-20 16:13:08 +0300

    chore: add default labels for configuration

--- src/services/config.ts
+++ src/services/config.ts
@@ -14,7 +14,60 @@ export const loadConfig = async (context: Context): Promise<Record<string, any>>
 						label: Joi.string(),
 					}),
 				)
-				.default(),
+				.default({
+					feat: {
+						pattern: "feat:|feat(.*):",
+						label: "Type: Feature",
+					},
+					fix: {
+						pattern: "fix:|fix(.*):",
+						label: "Type: Bugfix",
+					},
+					polish: {
+						pattern: "polish:|polish(.*):",
+						label: "Type: Polish",
+					},
+					docs: {
+						pattern: "docs:|docs(.*):",
+						label: "Type: Documentation",
+					},
+					style: {
+						pattern: "style:|style(.*):",
+						label: "Type: Style",
+					},
+					refactor: {
+						pattern: "refactor:|refactor(.*):",
+						label: "Type: Refactor",
+					},
+					perf: {
+						pattern: "perf:|perf(.*):",
+						label: "Type: Performance",
+					},
+					test: {
+						pattern: "test:|test(.*):",
+						label: "Type: Test",
+					},
+					workflow: {
+						pattern: "workflow:|workflow(.*):",
+						label: "Type: Workflow",
+					},
+					ci: {
+						pattern: "ci:|ci(.*):",
+						label: "Type: Continuous Integration",
+					},
+					chore: {
+						pattern: "chore:|chore(.*):",
+						label: "Type: Chore",
+					},
+					types: {
+						pattern: "types:|types(.*):",
+						label: "Type: Types",
+					},
+					release: {
+						pattern: "release:|release(.*):",
+						label: "Type: Release",
+					},
+				}),
 		})
 			.unknown(true)
 			.default(),
